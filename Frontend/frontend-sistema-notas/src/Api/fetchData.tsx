4b0c2948469f1b7aeab768c0b69bc9f8e5a1541f
dreisdev <dreis.dev@outlook.com>
2024-01-05 17:25:46 -0300

    deploy Frontend Netlify

--- Frontend/frontend-sistema-notas/src/Api/fetchData.tsx
+++ Frontend/frontend-sistema-notas/src/Api/fetchData.tsx
@@ -1,7 +1,7 @@
 import axios, { AxiosInstance } from "axios";
 
 const api: AxiosInstance = axios.create({
-  baseURL: "https://sistema-notas-api.netlify.app",
+  baseURL: "https://sistema-notas-api.cyclic.app",
 });
 
 export default api;
